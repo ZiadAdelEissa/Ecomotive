e7dbbbce099e7c5d99eb9aefc0579316ded08947
ZiadAdelEissa <ziadadel12321@hotmail.com>
2024-12-11 01:35:54 +0200

    edit video

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,9 +4,9 @@ import headv from '../imgs/headv.mp4';
 export default function Header() {
   return (
     <div className='head flex justify-center items-center w-full h-fit relative z-10'>
-      <video className=' object-cover h-full max-md:h-auto max-md:w-full' src={headv} autoPlay muted loop playsinline preload="none" type='video/mp4' />
+      <video className=' object-cover h-full max-md:h-auto max-md:w-full absolute -z-10 visible ' src={headv} autoPlay muted loop playsinline preload="none" type='video/mp4' />
       {/* <img src={headimg} alt='background-img' className='head-img object-cover w-full h-[400px]'/> */}
-      <div className='head-text flex flex-col  justify-center items-center absolute z-20 w-[350px] h-fit p-2  '>
+      <div className='head-text flex flex-col  justify-center items-center  absolute z-30 w-[350px] h-fit p-2  '>
       <h1 className='text-4xl font-bold '>Eco<span className='text-orange-600'>Motive</span></h1>
       <h2 className='text-xl text-white'>The Place</h2>
         <p className='text-lg text-slate-400'>That brings back the luster of your car </p>
