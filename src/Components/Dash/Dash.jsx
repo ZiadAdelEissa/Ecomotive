110565039bd3711d7c454170430523d71c9463bd
ZiadAdelEissa <ziadadel12321@hotmail.com>
2025-02-23 16:57:45 +0200

    fix video tag with iphone

--- src/Components/Dash/Dash.jsx
+++ src/Components/Dash/Dash.jsx
@@ -1,35 +1,59 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 export default function Dash() {
-    const [model,setModel] = useState("");
-    const [year, setYear] = useState("");
-    const [image, setImage] = useState();
-    const upload = ()=>{
-        const frormData = new FormData()
-        frormData.append('model',model);
-        frormData.append('year',year);
-        frormData.append('file',image)
-        axios.post('http://localhost:8081/create', frormData).then(res=>console.log(res)).catch(err=>console.log(err))
-    }
+  const [model, setModel] = useState("");
+  const [year, setYear] = useState("");
+  const [image, setImage] = useState();
+  const upload = () => {
+    const frormData = new FormData();
+    frormData.append("model", model);
+    frormData.append("year", year);
+    frormData.append("file", image);
+    axios
+      .post("http://localhost:8081/create", frormData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className='adds flex flex-col w-full h-full bg-slate-500 '>
-        <div className='flex flex-col gap-5 justify-around'>
-      <label>Car model</label>
-      <input type='text' className=' bg-black text-white' placeholder='Enter model' autoComplete='off'
-      onChange={(e)=> setModel(e.target.value)} />
-        </div>
-        <div className='flex flex-col gap-5 justify-around'>
-      <label>model year</label>
-      <input type='text' className=' bg-black text-white' placeholder='Enter model' autoComplete='off'
-      onChange={(e)=> setYear(e.target.value)} />
-        </div>
-        <div className='flex flex-col gap-5 justify-around'>
-      <label>add image</label>
-      <input type='file' className=' bg-black text-white' placeholder='Enter model' autoComplete='off' onChange={(e)=> setImage(e.target.files[0])} />
-        </div>
-        <div className='flex flex-col gap-5 justify-around'>
-      <button className=' w-full text-white bg-black hover:bg-slate-200' onClick={upload}>Add to dashboard</button>
+    <div className="adds flex flex-col w-full h-full bg-slate-500 ">
+      <div className="flex flex-col gap-5 justify-around">
+        <label>Car model</label>
+        <input
+          type="text"
+          className=" bg-black text-white"
+          placeholder="Enter model"
+          autoComplete="off"
+          onChange={(e) => setModel(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-5 justify-around">
+        <label>model year</label>
+        <input
+          type="text"
+          className=" bg-black text-white"
+          placeholder="Enter model"
+          autoComplete="off"
+          onChange={(e) => setYear(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col gap-5 justify-around">
+        <label>add image</label>
+        <input
+          type="file"
+          className=" bg-black text-white"
+          placeholder="Enter model"
+          autoComplete="off"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+      </div>
+      <div className="flex flex-col gap-5 justify-around">
+        <button
+          className=" w-full text-white bg-black hover:bg-slate-200"
+          onClick={upload}
+        >
+          Add to dashboard
+        </button>
       </div>
     </div>
-  )
+  );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,23 +5,21 @@ export default function Header() {
   return (
     <div className="head flex justify-center items-center w-full h-fit relative z-10">
       <video
-        className=" object-cover w-full h-full max-md:h-full max-md:w-full"
-        src={headv}
+        autoPlay="true"
         muted
+        noaudiotrack
         loop
-        playsInline
-        crossOrigin="anonymous"
-        poster={headimg}
-        alt="background-video"
-        style={{ objectFit: "cover" }}
-        preload="false"
-        autoPlay
-        type="video/mp4"
-      />
+        nofullscreen
+        playsinline="true"
+        className=" object-cover w-full h-full max-md:h-full max-md:w-full"
+      >
+        <source src={headv} type="video/mp4" />
+      </video>
+
       {/* <img src={headimg} alt='background-img' className='head-img object-cover w-full h-[400px]'/> */}
       <div className="head-text flex flex-col  justify-center items-center  absolute z-30 w-[350px] h-fit p-2  ">
         <h1 className="text-4xl font-bold ">
-          Eco<span className="text-orange-600">Motive</span>
+          Eco<span className="text-orange-600 ">Motive</span>
         </h1>
         <h2 className="text-xl text-white">The Place</h2>
         <p className="text-lg text-slate-400">
